267914ce9adb280caea06106b36f0a4b0ad6629a
saviosantanna <saviosantannadesa@gmail.com>
2022-08-07 22:48:23 -0300

    megasena

--- modulo 2/dom/mega-sena-v1/js/script.js
+++ modulo 2/dom/mega-sena-v1/js/script.js
@@ -1,19 +1,43 @@
 var state = { board:[], currentGame:[], savedGames:[]};
 
 function start(){
-    addNumberToGame(1);
-    addNumberToGame(2);
-    addNumberToGame(3);
-    addNumberToGame(4);
-    addNumberToGame(5);
-    saveGame();
-    addNumberToGame(6);
-    saveGame();
-    saveGame();
+    createBoard();
+    NewGame();
+    render();
+}
 
+function render(){
+    renderBoard();
+}
 
-    console.log(state.currentGame);
-    console.log(state.savedGames);
+function renderBoard(){
+    var divBoard = document.querySelector('#megasena-board');
+    divBoard.innerHTML = '';
+
+    var ulNumbers = document.createElement('ul');
+
+    for (var i=0; i<state.board.length; i++){
+        var currentNumber = state.board[i];
+
+        var liNumber = document.createElement('li')
+        liNumber.textContent = currentNumber;
+
+        ulNumbers.appendChild(liNumber);
+    }
+
+    divBoard.appendChild(ulNumbers);
+}
+
+function NewGame(){
+    resetGame();
+    render();
+}
+
+function createBoard(){
+    state.board = [];
+    for (var i=1; i<=60; i++){
+        state.board.push(i);
+    }
 }
 
 function addNumberToGame(numberToAdd){
@@ -68,6 +92,10 @@ function saveGame(){
 
 function isGameComplete(){
     return state.currentGame.length ===6;
+}
+
+function resetGame(){
+    state.currentGame =[];
 }
 
 start();
